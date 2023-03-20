9f39b026769c7f09ad68448f9946775af7233025
Lucas Rocha <lucas@lucas.local>
2023-03-19 22:23:49 -0300

    conectando com servidor real

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 function connectToDatabase() {
-    mongoose.connect("mongodb://localhost:27017/usuarios", {
+    mongoose.connect("mongodb://admin:XXXXXXXX@89.116.214.208:27017/usuarios?authSource=admin", {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     }).then(() => {
