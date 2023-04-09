ee8b21eb939d39a302701805f97d00f0462ff4b3
Lucas Rocha <lucas@lucas.local>
2023-04-08 23:58:40 -0300

    Permitindo acesso externo

--- index.js
+++ index.js
@@ -31,10 +31,17 @@ app.get("/", (req, res) => {
   //   res.send(resposta);
   // })
   res.send(
-    {message: "Seja bem-vindo a nossa pizzaria!"}
+    { message: "Seja bem-vindo a nossa pizzaria!" }
   );
 });
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 app.use("/usuario", usuario);
 app.use("/produto", produto);
 app.use("/categoria", categoria);
